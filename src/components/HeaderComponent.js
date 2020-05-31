1a041d184162e8eeeea13b617b734fb463956f66
Pallavi Sharma <pallavisharma2811@gmail.com>
2020-05-31 17:36:21 +0530

    React Router

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -1,13 +1,47 @@
 import React, { Component } from 'react';
-import { Navbar, NavbarBrand, Jumbotron } from 'reactstrap';
+import { Nav, Navbar, NavbarBrand, NavbarToggler, Collapse, NavItem, Jumbotron } from 'reactstrap';
+import { NavLink } from 'react-router-dom';
 
 class Header extends Component {
+  constructor(props) {
+    super(props);
+
+    this.toggleNav = this.toggleNav.bind(this);// this.toggleNav (JS variable) pointing to toggleNav method(need to bind method to JS variable,to access method in JSX)
+    this.state = {// we needed to maintain local state in Collapse reactstrap comp
+      isNavOpen: false
+    };
+  }
+
+  toggleNav() {
+    this.setState({//we are negating the value of isNavOpen whatever it is
+      isNavOpen: !this.state.isNavOpen
+    });
+  }
     render() {
-      return(
+      return(// isOpen is boolean attribute, display will display when true. Will toggle by click.
       <React.Fragment>
-        <Navbar dark>
+        <Navbar dark expand="md">
           <div className="container">
-              <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
+            <NavbarToggler onClick={this.toggleNav} />
+              <NavbarBrand className="mr-auto" href="/">
+              <img src='assets/images/logo.png' height="30" width="41" alt='Ristorante Con Fusion' />
+              </NavbarBrand>
+              <Collapse isOpen={this.state.isNavOpen} navbar>
+                <Nav navbar>
+                  <NavItem>
+                    <NavLink className="nav-link"  to='/home'><span className="fa fa-home fa-lg"></span> Home</NavLink>
+                  </NavItem>
+                  <NavItem>
+                      <NavLink className="nav-link" to='/aboutus'><span className="fa fa-info fa-lg"></span> About Us</NavLink>
+                  </NavItem>
+                  <NavItem>
+                    <NavLink className="nav-link"  to='/menu'><span className="fa fa-list fa-lg"></span> Menu</NavLink>
+                  </NavItem>
+                  <NavItem>
+                    <NavLink className="nav-link" to='/contactus'><span className="fa fa-address-card fa-lg"></span> Contact Us</NavLink>
+                  </NavItem>            
+                </Nav>
+              </Collapse>
           </div>
         </Navbar>
         <Jumbotron>
