44674a8b09621a062e8c14655eb1ad90f7446e1a
Pallavi Sharma <pallavisharma2811@gmail.com>
2020-06-01 16:41:33 +0530

    Single Page Applications Part 1

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,10 +1,17 @@
 import React, { Component } from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom';
+
 import Menu from './MenuComponent';
-import { DISHES } from '../shared/dishes';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import Home from './HomeComponent';
+import Contact from './ContactComponent';
+import { DISHES } from '../shared/dishes';
+import { COMMENTS } from '../shared/comments';
+import { LEADERS } from '../shared/leaders';
+import { PROMOTIONS } from '../shared/promotions';
+
+
 
 //Container Component
 class Main extends Component {
@@ -13,6 +20,9 @@ class Main extends Component {
     super(props);
     this.state = {
         dishes: DISHES,
+        comments: COMMENTS,
+        leaders: LEADERS,
+        promotions: PROMOTIONS
     };
   }
 
@@ -20,6 +30,9 @@ class Main extends Component {
     const HomePage = () => {//explicitly declare the functional component in 1st route and inline declaration in 2nd rout
       return(
           <Home 
+          dish={this.state.dishes.filter(dish => dish.featured)[0]}
+          promotion={this.state.promotions.filter((promo) => promo.featured)[0]}
+          leader={this.state.leaders.filter((leader) => leader.featured)[0]}
           />
       );
     }
@@ -29,6 +42,7 @@ class Main extends Component {
         <Switch>
           <Route path ="/home" component={HomePage}/>
           <Route exact path ="/menu" component={()=><Menu dishes={this.state.dishes}/>}/>
+          <Route exact path='/contactus' component={Contact} />} />
           <Redirect to = "/home" />
         </Switch>
         <Footer />
